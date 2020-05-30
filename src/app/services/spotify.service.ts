f2143a22725f873b23c2afaee90c087e404e7628
franco <francohuanaco@gmail.com>
2020-05-29 23:12:10 -0400

    Componente de busqueda de artistas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,9 +11,18 @@ export class SpotifyService {
 	getNewReleases() {
 		const headers = new HttpHeaders({
 			Authorization:
-				'Bearer BQDIcCtQbdLn2zW3v6W4pZjeche98tctcCl-6XYGSoI-d5-OgG-LcFPbtKTk7EjC4m0cXnrCNe9w1uive0iwWuoxJNvAHCDE4d_44uthh4YEYyN5Q0EuE177FkBktr56_LdUB1Jn'
+				'Bearer BQDb0ZzQOoC7GXXoZ4QfaOXSLgmW-fBniW2ruwRY5q-4WIpyw9Fx73_PUyTedI9G8GICRVZelwE7NeDwe0ClXdCHN2KJD15SqatH1Xssi4Kh9a2qJro_o6oorOx8N5w27sf9OFwI'
 		});
 
 		return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+	}
+
+	getArtista(termino: string) {
+		const headers = new HttpHeaders({
+			Authorization:
+				'Bearer BQBlsBBL0vBBbXNiEPbWdZiOVo5QHxrG2zzz2MOpmUeNUyvbHfH2dVK56n1qEsNd1eIUDa8-JBb2aF_JkV5sWaHXw8YzdSkG02ootXrHgouFhIxPb7ZivbRDCM22xNH_eect_PXZ'
+		});
+
+		return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers });
 	}
 }
