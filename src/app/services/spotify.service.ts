4bf3f39bef217e7bd98c559d2f09f005dc859c0b
franco <francohuanaco@gmail.com>
2020-05-30 22:40:40 -0400

    Servicio de Spotify Optimizado

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,25 +9,22 @@ export class SpotifyService {
 	constructor(private http: HttpClient) {
 		console.log('Servicio de spotifi corriendo');
 	}
-	getNewReleases() {
+
+	getQuery(query: string) {
+		const url = `https://api.spotify.com/v1/${query}`;
 		const headers = new HttpHeaders({
 			Authorization:
-				'Bearer BQDCId-U64XrIdIG0Hcrzb8yuxTKiyDL7sFRVakYO9Fd8IJhg2NLMLBkhL3XNaHSHaRSWuRnhC6PxuyLVxS3_YauTIUdyKx5UDHLFpHs4Y5yQXoK9yX1PtVRtw8qeTaVL5ZmQlmt'
+				'Bearer BQB7UcgkSKSJFNcOrrRFKHTH96UsoK2AsiQPd22mEuRwkRha0c0qX82PNiMGRodHH-00IxDBGkRLlmwd2PDPPnImDqclg2YKYAFUEwwmGnslJ7sHQZVAW_HRafCLbZ6MKJvFRGdQ'
 		});
 
-		return this.http
-			.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-			.pipe(map((data) => data['albums']['items']));
+		return this.http.get(url, { headers });
+	}
+
+	getNewReleases() {
+		return this.getQuery('browse/new-releases').pipe(map((data) => data['albums']['items']));
 	}
 
 	getArtista(termino: string) {
-		const headers = new HttpHeaders({
-			Authorization:
-				'Bearer BQDCId-U64XrIdIG0Hcrzb8yuxTKiyDL7sFRVakYO9Fd8IJhg2NLMLBkhL3XNaHSHaRSWuRnhC6PxuyLVxS3_YauTIUdyKx5UDHLFpHs4Y5yQXoK9yX1PtVRtw8qeTaVL5ZmQlmt'
-		});
-
-		return this.http
-			.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
-			.pipe(map((data) => data['artists']['items']));
+		return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(map((data) => data['artists']['items']));
 	}
 }
